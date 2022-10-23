69b9cd0321ca09c1c1b0df9f4d39d75ab5a4b538
Glory13501~ <adeyemiglr@gmail.com>
2022-10-23 13:02:13 +0100

    updated

--- src/Components/LeftBar/LeftBar.jsx
+++ src/Components/LeftBar/LeftBar.jsx
@@ -1,132 +1,128 @@
-import React, { useContext, useState } from "react";
-import { Context } from "../../Context/Context";
+import React, { useContext, useState } from 'react';
+import { Context } from '../../Context/Context';
 import {
-  Home,
-  Menu,
-  CancelOutlined,
-  Info,
-  Contacts,
-  PostAdd,
-  Add,
-  ContactSupport,
-  ExitToApp,
-} from "@material-ui/icons";
-import { makeStyles } from "@material-ui/core";
-import { Link, useLocation } from "react-router-dom";
+	Home,
+	Menu,
+	CancelOutlined,
+	Info,
+	Contacts,
+	PostAdd,
+	Add,
+	ContactSupport,
+	ExitToApp,
+} from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core';
+import { Link, useLocation } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
-  icon: {
-    // color: (props) => props.color,
-  },
+	icon: {
+		// color: (props) => props.color,
+	},
 }));
 
 export const LeftBar = () => {
-  const [open, setOpen] = useState(false);
-  const { theme } = useContext(Context);
-  const classes = useStyles({ color: theme.primary });
-  const { pathname } = useLocation();
-  const location = pathname.split("/")[1];
+	const [open, setOpen] = useState(false);
+	const { theme } = useContext(Context);
+	const classes = useStyles({ color: theme.primary });
+	const { pathname } = useLocation();
+	const location = pathname.split('/')[1];
 
-  const styles = {
-    name: {
-      display: open ? "block" : "none",
-    },
-    leftItem: (name) => {
-      return {
-        backgroundColor:
-          name === location ? "rgba(255, 255, 255, 0.371)" : "inherit",
-      };
-    },
-  };
+	const styles = {
+		name: {
+			display: open ? 'block' : 'none',
+		},
+		leftItem: (name) => {
+			return {
+				backgroundColor:
+					name === location
+						? 'rgba(255, 255, 255, 0.371)'
+						: 'inherit',
+			};
+		},
+	};
 
-  return (
-    <div style={{ backgroundColor: theme.primary }} className='leftBar'>
-      <div className='left-item menu'>
-        {" "}
-        <CancelOutlined
-          style={{
-            transform: open ? "rotateX(0deg)" : "rotateX(90deg)",
-          }}
-          onClick={() => {
-            setOpen(false);
-          }}
-          className='menu-icon'
-        />
-        <Menu
-          style={{
-            transform: open ? "rotateX(90deg)" : "rotateX(0deg)",
-          }}
-          onClick={() => {
-            setOpen(true);
-          }}
-          className='menu-icon'
-        />
-      </div>
+	return (
+		<div style={{ backgroundColor: theme.primary }} className='leftBar'>
+			<div style={{ backgroundColor: theme.primary }} className='left-item menu'>
+				{' '}
+				<CancelOutlined
+					style={{
+						transform: open ? 'rotateX(0deg)' : 'rotateX(90deg)',
+					}}
+					onClick={() => {
+						setOpen(false);
+					}}
+					className='menu-icon'
+				/>
+				<Menu
+					style={{
+						transform: open ? 'rotateX(90deg)' : 'rotateX(0deg)',
+					}}
+					onClick={() => {
+						setOpen(true);
+					}}
+					className='menu-icon'
+				/>
+			</div>
 
-      <Link
-        style={{ marginTop: "70px", ...styles.leftItem("") }}
-        className='left-item Link'
-        to='/'
-      >
-        {" "}
-        <Home className={classes.icon && "icons"} />{" "}
-        <span style={styles.name}>Home</span>{" "}
-      </Link>
+			<Link
+				style={{ marginTop: '70px', ...styles.leftItem('') }}
+				className='left-item Link'
+				to='/'>
+				{' '}
+				<Home className={classes.icon && 'icons'} />{' '}
+				<span style={styles.name}>Home</span>{' '}
+			</Link>
 
-      <Link
-        style={styles.leftItem("about")}
-        className='left-item Link'
-        to='/about'
-      >
-        {" "}
-        <Info className={classes.icon && "icons"} />{" "}
-        <span style={styles.name}>About Us </span>
-      </Link>
+			<Link
+				style={styles.leftItem('about')}
+				className='left-item Link'
+				to='/about'>
+				{' '}
+				<Info className={classes.icon && 'icons'} />{' '}
+				<span style={styles.name}>About Us </span>
+			</Link>
 
-      <Link
-        style={styles.leftItem("contact")}
-        className='left-item Link'
-        to='/contact'
-      >
-        {" "}
-        <ContactSupport className={classes.icon && "icons"} />
-        <span style={styles.name}>Contact Us </span>
-      </Link>
+			<Link
+				style={styles.leftItem('contact')}
+				className='left-item Link'
+				to='/contact'>
+				{' '}
+				<ContactSupport className={classes.icon && 'icons'} />
+				<span style={styles.name}>Contact Us </span>
+			</Link>
 
-      <Link
-        style={styles.leftItem("writerApplication")}
-        className='left-item Link'
-        to='/writerApplication'
-      >
-        {" "}
-        <PostAdd className={classes.icon && "icons"} />{" "}
-        <span style={styles.name}>Apply To Be A Writer </span>
-      </Link>
+			<Link
+				style={styles.leftItem('writerApplication')}
+				className='left-item Link'
+				to='/writerApplication'>
+				{' '}
+				<PostAdd className={classes.icon && 'icons'} />{' '}
+				<span style={styles.name}>Apply To Be A Writer </span>
+			</Link>
 
-      <Link
-        style={styles.leftItem("writers")}
-        className='left-item Link'
-        to='/writers'
-      >
-        {" "}
-        <Contacts className={classes.icon && "icons"} />{" "}
-        <span style={styles.name}>Writers </span>
-      </Link>
+			<Link
+				style={styles.leftItem('writers')}
+				className='left-item Link'
+				to='/writers'>
+				{' '}
+				<Contacts className={classes.icon && 'icons'} />{' '}
+				<span style={styles.name}>Writers </span>
+			</Link>
 
-      <Link
-        style={styles.leftItem("post")}
-        className='left-item Link'
-        to='/post'
-      >
-        {" "}
-        <Add className={classes.icon && "icons"} />{" "}
-        <span style={styles.name}>Write </span>
-      </Link>
+			<Link
+				style={styles.leftItem('post')}
+				className='left-item Link'
+				to='/post'>
+				{' '}
+				<Add className={classes.icon && 'icons'} />{' '}
+				<span style={styles.name}>Write </span>
+			</Link>
 
-      <div className='left-item' to='/'>
-        <ExitToApp className={classes.icon && "icons"} />{" "}
-        <span style={styles.name}>LogOut </span>
-      </div>
-    </div>
-  );
+			<div className='left-item' to='/'>
+				<ExitToApp className={classes.icon && 'icons'} />{' '}
+				<span style={styles.name}>LogOut </span>
+			</div>
+		</div>
+	);
 };
--- src/Components/TopBar/TopBar.jsx
+++ src/Components/TopBar/TopBar.jsx
@@ -1,137 +1,160 @@
-import React, { useContext, useState } from "react";
-import { Context } from "../../Context/Context";
-import { Cancel, Notifications,  Search } from "@material-ui/icons";
-import { AppBar, Button, makeStyles } from "@material-ui/core";
-import sun from "../../images/icon-sun.svg";
-import moon from "../../images/icon-moon.svg";
-import { data } from "../..//DummyData/commentsData";
+import React, { useContext, useState } from 'react';
+import { Context } from '../../Context/Context';
+import { CancelOutlined, Notifications, Search } from '@material-ui/icons';
+import { AppBar, Button, makeStyles, Menu } from '@material-ui/core';
+import sun from '../../images/icon-sun.svg';
+import moon from '../../images/icon-moon.svg';
+import { data } from '../..//DummyData/commentsData';
 
 const useStyles = makeStyles((theme) => ({
-  searchIcon: {
-    cursor: "pointer",
-    color: "white",
-    fontSize: "20px",
-  },
+	searchIcon: {
+		cursor: 'pointer',
+		color: 'white',
+		fontSize: '20px',
+	},
 }));
 
 export const TopBar = ({ setOpenLoginModal, setOpenNotifications }) => {
-  const [openSearch, setOpenSearch] = useState(false);
-  const { light, dark, theme, themeDispatch } = useContext(Context);
-  const classes = useStyles();
-  const user = data.currentUser;
+	const [openSearch, setOpenSearch] = useState(false);
+	const { light, dark, theme, themeDispatch } = useContext(Context);
+	const classes = useStyles();
+	const user = data.currentUser;
+	const mobileScreen = 550;
+	let screenWidth = window.screen.availWidth;
 
-  return (
-    <AppBar
-      style={{ backgroundColor: theme.primary }}
-      className='topBar'
-      position='fixed'
-    >
-      <div className='topBar-container'>
-        <div className='logo'>
-          Unique <span>Writers</span>{" "}
-        </div>
-        <div className='topbar-second-container'>
-          <div
-            style={{
-              backgroundColor: openSearch
-                ? "rgba(255, 255, 255, 0.319)"
-                : "transparent",
-            }}
-            className='search-container'
-          >
-            <Search
-              onClick={() => setOpenSearch(true)}
-              className={classes.searchIcon}
-            />
-            <input
-              style={{
-                // display: openSearch ? "block" : "none",
-                width: openSearch ? "50vw" : 0,
-              }}
-              type='search'
-              placeholder='Search....'
-            />
-            <div
-              style={{
-                display: openSearch ? "block" : "none",
-              }}
-              onClick={() => setOpenSearch(false)}
-              className='cancel'
-            >
-              <Cancel className='cancel-icon' />
-            </div>
-          </div>
+	return (
+		<AppBar
+			style={{ backgroundColor: theme.primary }}
+			className='topBar'
+			position='fixed'>
+				<Menu
+					// style={{
+					// 	transform: open ? 'rotateX(90deg)' : 'rotateX(0deg)',
+					// }}
+					// onClick={() => {
+					// 	setOpen(true);
+					// }}
+					// className='menu-icon'
+				/>
+			<div
+				style={{
+					justifyContent:
+						screenWidth < mobileScreen && openSearch
+							? 'center'
+							: 'space-between',
+				}}
+				className='topBar-container'>
+				<div
+					style={{
+						display:
+							screenWidth < mobileScreen && openSearch
+								? 'none'
+								: 'block',
+					}}
+					className='logo'>
+					Unique <span>Writers</span>{' '}
+				</div>
 
-          <div
-            style={{
-              display: openSearch ? "none" : "flex",
-            }}
-            onClick={() => {
-              if (light) {
-                themeDispatch({ type: "DARK_MODE" });
-              } else if (dark) {
-                themeDispatch({ type: "LIGHT_MODE" });
-              }
-            }}
-            className='top-theme-switcher'
-          >
-            <div
-              style={{
-                transform: light ? "translateX(0)" : "translateX(95%)",
-              }}
-              className='ball'
-            ></div>
-            <div className='theme-container'>
-              <img src={moon} alt='moon' />
-            </div>
-            <div className='theme-container'>
-              <img src={sun} alt='sun' />
-            </div>
-          </div>
+				<div
+					className='topbar-second-container'
+					style={{ width: openSearch && 'auto' }}>
+					<div
+						style={{
+							backgroundColor: openSearch
+								? 'rgba(255, 255, 255, 0.319)'
+								: 'transparent',
+						}}
+						className='search-container'>
+						<Search
+							onClick={() => setOpenSearch(true)}
+							className={classes.searchIcon}
+						/>
+						<input
+							style={{
+								width:
+									screenWidth < mobileScreen && openSearch
+										? '80vw'
+										: openSearch
+										? '50vw'
+										: 0,
+							}}
+							type='text'
+							placeholder='Search....'
+						/>
+						<div
+							style={{
+								display: openSearch ? 'block' : 'none',
+							}}
+							onClick={() => setOpenSearch(false)}
+							className='cancel'>
+							<CancelOutlined className='cancel-icon' />
+						</div>
+					</div>
 
-          {/* <Button
-          type='submit'
-          variant='contained'
-          size='small'
-          onClick={() => setOpenLoginModal(true)}
-          style={{
-            backgroundColor: theme.secondary,
-            color: "white",
-            borderRadius: "10px",
-            display: openSearch ? "none" : "flex",
-          }}
-        >
-          login
-        </Button> */}
+					<div
+						style={{
+							display: openSearch ? 'none' : 'flex',
+						}}
+						onClick={() => {
+							if (light) {
+								themeDispatch({ type: 'DARK_MODE' });
+							} else if (dark) {
+								themeDispatch({ type: 'LIGHT_MODE' });
+							}
+						}}
+						className='top-theme-switcher'>
+						<div
+							style={{
+								transform: light
+									? 'translateX(5%)'
+									: 'translateX(95%)',
+							}}
+							className='ball'></div>
+						<div className='theme-container'>
+							<img src={moon} alt='moon' />
+						</div>
+						<div className='theme-container'>
+							<img src={sun} alt='sun' />
+						</div>
+					</div>
 
-          <div
-            style={{
-              display: openSearch ? "none" : "flex",
-            }}
-            className='top-notification-con'
-            onClick={()=>{
-                setOpenNotifications(true)
-              }}
-          >
-            <div 
-              className='top-notification-main-container'
-              
-            >
-              <Notifications />
-              <div className='badge'>3</div>
-            </div>
-          </div>
+					{/* <Button
+						type='submit'
+						variant='contained'
+						size='small'
+						onClick={() => setOpenLoginModal(true)}
+						style={{
+							backgroundColor: theme.secondary,
+							color: 'white',
+							borderRadius: '10px',
+							display: openSearch ? 'none' : 'flex',
+						}}>
+						login
+					</Button> */}
 
-          <div
-            style={{
-              display: openSearch ? "none" : "flex",
-            }}
-            className='top-profile-container'
-          >
-            <img src={user.user.image.png} alt='' />
-          </div>
-        </div>
-      </div>
-    </AppBar>
-  );
+					<div
+						style={{
+							display: openSearch ? 'none' : 'flex',
+						}}
+						className='top-notification-con'
+						onClick={() => {
+							setOpenNotifications(true);
+						}}>
+						<div className='top-notification-main-container'>
+							<Notifications />
+							<div className='badge'>3</div>
+						</div>
+					</div>
+
+					<div
+						style={{
+							display: openSearch ? 'none' : 'flex',
+						}}
+						className='top-profile-container'>
+						<img src={user.user.image.png} alt='' />
+					</div>
+				</div>
+			</div>
+		</AppBar>
+	);
 };
